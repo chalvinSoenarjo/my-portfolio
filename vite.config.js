d6653334daf1a9c5001f762e58b7bc7f2b34d2c1
chalvinSoenarjo <chalssensi@gmail.com>
2026-04-29 15:03:15 -0700

    Set base path to /dist/

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/my-portfolio/', // <-- THIS IS THE LINE YOU NEED TO ADD
+  base: '/my-portfolio/dist/',
   plugins: [react()],
 })
